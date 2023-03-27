e817fd8e021ea8dd61a334254283108c7ca1605d
faresdu123 <falduhailana@gmail.com>
2023-03-27 13:48:24 +0300

    trying and trying

--- lib/ui/profile/profile_view.dart
+++ lib/ui/profile/profile_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gdsc_app/ui/profile/profile_viewmodel.dart';
 import 'package:stacked/stacked.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({Key? key}) : super(key: key);
@@ -11,7 +12,85 @@ class ProfileView extends StatelessWidget {
         viewModelBuilder: () => ProfileViewModel(),
         builder: (context, viewmodel, _) {
           return Scaffold(
-            appBar: AppBar(title: Text('File modified by fares')),
+            body: SafeArea(
+              child: ListView(
+                children: [
+                  Container(
+                    color: Color(0xFFBFDEF5),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            'الملف الشخصي',
+                            style: GoogleFonts.cairo(
+                              textStyle: TextStyle(fontSize: 30.0),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 100.0,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                            child: Card(
+                              color: Color(0xFFBFDEF5),
+                              elevation: 5.0,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      TextButton(
+                                          onPressed: () {},
+                                          child: Icon(
+                                            Icons.add_circle,
+                                            color: Colors.black,
+                                          )),
+                                    ],
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                  ),
+                                  Row(
+                                    children: [
+                                      const Padding(
+                                        padding: EdgeInsets.only(
+                                            right: 20.0, bottom: 15.0),
+                                        child: CircleAvatar(
+                                          backgroundColor: Colors.red,
+                                          radius: 40.0,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 60.0,
+                                      ),
+                                      Text(
+                                        'فارس الدحيلان',
+                                        style: GoogleFonts.cairo(
+                                          textStyle: TextStyle(
+                                            fontSize: 20.0,
+                                            fontWeight: FontWeight.w900,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.only(bottom: 10.0),
+                                        child: Text('قائد لجنة المطورين'),
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ]),
+                  ),
+                ],
+              ),
+            ),
           );
         });
   }
--- lib/ui/profile/profile_viewmodel.dart
+++ lib/ui/profile/profile_viewmodel.dart
@@ -1,3 +1,22 @@
+import 'package:gdsc_app/core/models/committee.dart';
+import 'package:gdsc_app/core/models/member.dart';
 import 'package:stacked/stacked.dart';
 
-class ProfileViewModel extends BaseViewModel {}
+class ProfileViewModel extends BaseViewModel {
+Member member = Member(id: '443', sID: '12345', name: 'fares',committee: Committee(id: '443', name: 'Tech',coLeaderID: '4321',leaderID: '3123'));
+  String fares(){
+    return 'Profile name';
+  }
+  getProfileInfo(){
+    return member;
+  }
+  String getCommitteeStatus(){
+    if(member.committee?.leaderID == member.id){
+      return 'leader of Tech';
+    }else if(member.committee?.coLeaderID == member.id){
+      return 'Co-Leader of Tech';
+    }else{
+      return 'member of Tech';
+    }
+  }
+}
