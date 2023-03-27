eb9202468148bb0c4a6abd1a326abcf4be96bd75
M01010 <ihaamoodi@gmail.com>
2023-03-28 00:11:56 +0300

    minor fixes

--- lib/ui/events/events_view.dart
+++ lib/ui/events/events_view.dart
@@ -4,6 +4,8 @@ import 'package:gdsc_app/core/utils/constants.dart';
 import 'package:gdsc_app/ui/events/events_viewmodel.dart';
 import 'package:stacked/stacked.dart';
 
+TextStyle titlestyle =
+    const TextStyle(fontSize: 24, fontWeight: FontWeight.w700);
 class EventsView extends StatelessWidget {
   const EventsView({Key? key}) : super(key: key);
 
@@ -32,97 +34,84 @@ class Details extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Image(
-              image: AssetImage('assets/images/temp-events-img.png'),
-            ),
-            Text(
-              'مقدمة في علم البيانات',
-              style: titlestyle,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                EventDayBox(day: 21, month: 'Jan'),
-                EventInfoBox(
-                    topString: 'الاربعاء', bottomString: '6:00PM - 3:00PM'),
-                EventInfoBox(
-                    topString: 'الموقع', bottomString: 'بهو الجامعة'),
-                EventInfoBox(
-                    topString: 'المحاضر', bottomString: 'بسام البسام')
-              ],
-            ),
-            Divider(
-              height: 1,
-              color: Color(0xFF666C74).withOpacity(.5),
-            ),
-            Text(
-              'عن الفعالية',
-              style: titlestyle,
-            ),
-            Text(
-              'ما أصله؟ خلافاَ للاعتقاد السائد فإن لوريم إيبسوم ليس نصاَ عشوائياً، بل إن له جذور في الأدب اللاتيني الكلاسيكي منذ العام 45 قبل الميلاد، مما يجعله أكثر من عام في',
-              style: descriptionstyle,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                EventSignupButton(onPressed: () {}),
-                EventAttendees(
-                  seats: 18,
-                  members: [
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                    Member.anonymous(),
-                  ],
-                ),
-              ],
-            ),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Image(
+                image: AssetImage('assets/images/temp-events-img.png'),
+              ),
+              Text(
+                'مقدمة في علم البيانات',
+                style: titlestyle,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  EventDayBox(day: 21, month: 'Jan'),
+                  EventInfoBox(
+                      topString: 'الاربعاء', bottomString: '6:00PM - 3:00PM'),
+                  EventInfoBox(
+                      topString: 'الموقع', bottomString: 'بهو الجامعة'),
+                  EventInfoBox(
+                      topString: 'المحاضر', bottomString: 'بسام البسام')
+                ],
+              ),
+              Divider(
+                height: 1,
+                color: const Color(0xFF666C74).withOpacity(.5),
+              ),
+              Text(
+                'عن الفعالية',
+                style: titlestyle,
+              ),
+              const Text(
+                'ما أصله؟ خلافاَ للاعتقاد السائد فإن لوريم إيبسوم ليس نصاَ عشوائياً، بل إن له جذور في الأدب اللاتيني الكلاسيكي منذ العام 45 قبل الميلاد، مما يجعله أكثر من عام في',
+                style: TextStyle(
+                    fontSize: 20, fontWeight: FontWeight.w600, color: Color(0xFF666C74)),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  EventSignupButton(onPressed: () {}),
+                  EventAttendees(
+                    seats: 18,
+                    members: [
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                      Member.anonymous(),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-TextStyle titlestyle =
-    const TextStyle(fontSize: 24, fontWeight: FontWeight.w700);
-TextStyle topstyle = const TextStyle(fontSize: 14, fontWeight: FontWeight.w700);
-TextStyle numupstyle =
-    const TextStyle(fontSize: 12, fontWeight: FontWeight.w500);
-TextStyle numdownstyle =
-    const TextStyle(fontSize: 12, fontWeight: FontWeight.w700);
-TextStyle bottomstyle = const TextStyle(
-    fontSize: 12, fontWeight: FontWeight.w700, color: Color(0xFF666C74));
-TextStyle signupstyle = const TextStyle(
-    fontSize: 12, fontWeight: FontWeight.w700, color: Colors.white);
-TextStyle descriptionstyle = const TextStyle(
-    fontSize: 20, fontWeight: FontWeight.w600, color: Color(0xFF666C74));
-TextStyle daystyle =
-    const TextStyle(fontSize: 16, fontWeight: FontWeight.w700, height: 1);
 
 class EventAttendees extends StatelessWidget {
   const EventAttendees({Key? key, required this.members, required this.seats})
@@ -134,20 +123,22 @@ class EventAttendees extends StatelessWidget {
   Widget build(BuildContext context) {
     List<Widget> images = [];
     if (members.length > 3) {
-      images.add(CircleAvatar(
-          child: Text(
-            '+${members.length - 3}',
-            style: TextStyle(
-                fontSize: 10, fontWeight: FontWeight.w500, color: Colors.black),
-          ),
+      images.add(
+        CircleAvatar(
           backgroundColor: Colors.white,
           radius: 10,
+          child: Text(
+            '+${members.length - 3}',
+            style: const TextStyle(
+                fontSize: 10, fontWeight: FontWeight.w500, color: Colors.black),
+          ),
         ),
       );
     }
     for (int i = 0; i < 3 && i < members.length; i++) {
       images.add(CircleAvatar(
-        foregroundImage: AssetImage('assets/images/event-attendees.png'),
+        foregroundImage:
+            AssetImage(members[i].photo ?? 'assets/images/event-attendees.png'),
         backgroundColor: Constants.red,
         radius: 10,
       ));
@@ -158,11 +149,11 @@ class EventAttendees extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             const Image(image: AssetImage('assets/images/event-attendees.png')),
-            Padding(
-              padding: const EdgeInsets.only(right: 10),
+            const Padding(
+              padding: EdgeInsets.only(right: 10),
               child: Text(
                 'المشاركين',
-                style: numupstyle,
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
               ),
             ),
             Row(children: images),
@@ -170,7 +161,7 @@ class EventAttendees extends StatelessWidget {
         ),
         Text(
           'المقاعد المتبقية $seats',
-          style: numdownstyle,
+          style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w700),
         )
       ],
     );
@@ -188,8 +179,18 @@ class EventInfoBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(topString, style: topstyle),
-        Text(bottomString, style: bottomstyle)
+        Text(
+          topString,
+          style: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        Text(bottomString,
+            style: const TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w700,
+                color: Color(0xFF666C74)))
       ],
     );
   }
@@ -213,7 +214,7 @@ class EventDayBox extends StatelessWidget {
         color: const Color(0xFF666C74).withOpacity(.1),
       ),
       child: Center(
-        child: Text('$day\n$month', style: daystyle),
+        child: Text('$day\n$month', style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w700, height: 1)),
       ),
     );
   }
@@ -236,10 +237,11 @@ class EventSignupButton extends StatelessWidget {
         decoration: BoxDecoration(
             color: const Color(0xFF212A40).withOpacity(.9),
             borderRadius: BorderRadius.circular(10)),
-        child: Center(
+        child: const Center(
           child: Text(
             'احجز مقعدك',
-            style: signupstyle,
+            style:  TextStyle(
+                fontSize: 12, fontWeight: FontWeight.w700, color: Colors.white),
           ),
         ),
       ),
