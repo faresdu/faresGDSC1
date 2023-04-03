843bb344056b2b1df067faec9752d97cda67e962
M01010 <ihaamoodi@gmail.com>
2023-04-03 09:22:34 +0300

    used userservice

--- lib/ui/profile/profile_viewmodel.dart
+++ lib/ui/profile/profile_viewmodel.dart
@@ -1,6 +1,5 @@
 import 'package:gdsc_app/core/services/authentication_service.dart';
 import 'package:stacked/stacked.dart';
-import 'package:supabase/supabase.dart';
 import '../../core/app/app.locator.dart';
 import '../../core/models/member.dart';
 import '../../core/services/supabase_service.dart';
@@ -9,13 +8,11 @@ import '../../core/services/user_service.dart';
 class ProfileViewModel extends BaseViewModel {
   final authService = locator<AuthenticationService>();
   final supabaseService = locator<SupabaseService>();
-  // final userService = locator<UserService>();
+  final userService = locator<UserService>();
   late Member profile = Member.anonymous();
 
   void getProfile() async {
-    User? u = await authService.getCurrentUser();
-    // profile = await supabaseService.getMemberProfile(userService.user.id);
-    profile = await supabaseService.getMemberProfile(u!.id);
+    profile = await supabaseService.getMemberProfile(userService.user.id);
     notifyListeners();
   }
 
