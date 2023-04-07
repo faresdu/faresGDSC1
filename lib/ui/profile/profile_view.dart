49809014152c49a303757d6904049ad20a572fc6
faresdu123 <falduhailana@gmail.com>
2023-04-07 21:52:09 +0300

    profile view first version

--- lib/ui/profile/profile_view.dart
+++ lib/ui/profile/profile_view.dart
@@ -18,75 +18,144 @@ class ProfileView extends StatelessWidget {
                   Container(
                     color: Color(0xFFBFDEF5),
                     child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                            'الملف الشخصي',
-                            style: GoogleFonts.cairo(
-                              textStyle: TextStyle(fontSize: 30.0),
-                            ),
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        SizedBox(
+                            height: 30.0), // Add some space before the title
+                        Text(
+                          'الملف الشخصي',
+                          style: GoogleFonts.cairo(
+                            textStyle: TextStyle(fontSize: 30.0),
                           ),
-                          SizedBox(
-                            height: 100.0,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                            child: Card(
-                              color: Color(0xFFBFDEF5),
-                              elevation: 5.0,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    children: [
-                                      TextButton(
-                                          onPressed: () {},
-                                          child: Icon(
-                                            Icons.add_circle,
-                                            color: Colors.black,
-                                          )),
-                                    ],
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                  ),
-                                  Row(
-                                    children: [
-                                      const Padding(
-                                        padding: EdgeInsets.only(
-                                            right: 20.0, bottom: 15.0),
-                                        child: CircleAvatar(
-                                          backgroundColor: Colors.red,
-                                          radius: 40.0,
+                        ),
+                        SizedBox(height: 60.0),
+                        Padding(
+                          padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                          child: Card(
+                            color: Color(0xFFBFDEF5),
+                            elevation: 15.0,
+                            shadowColor: Colors.black,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(
+                                        Icons.edit,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    const Padding(
+                                      padding: EdgeInsets.only(
+                                        right: 20.0,
+                                        bottom: 15.0,
+                                      ),
+                                      child: CircleAvatar(
+                                        backgroundColor: Colors.red,
+                                        radius: 40.0,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 60.0,
+                                    ),
+                                    Text(
+                                      'فارس الدحيلان',
+                                      style: GoogleFonts.cairo(
+                                        textStyle: TextStyle(
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.w900,
                                         ),
                                       ),
-                                      SizedBox(
-                                        width: 60.0,
-                                      ),
-                                      Text(
-                                        'فارس الدحيلان',
-                                        style: GoogleFonts.cairo(
-                                          textStyle: TextStyle(
-                                            fontSize: 20.0,
-                                            fontWeight: FontWeight.w900,
-                                          ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.only(bottom: 10.0),
+                                      child: Text('عضو لجنة المطورين'),
+                                    )
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    SizedBox(width: 50),
+                                    Expanded(
+                                      child: Text(
+                                        '4',
+                                        style: TextStyle(
+                                          fontSize: 30.0,
+                                          fontWeight: FontWeight.bold,
                                         ),
                                       ),
-                                    ],
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.only(bottom: 10.0),
-                                        child: Text('قائد لجنة المطورين'),
-                                      )
-                                    ],
-                                  )
-                                ],
-                              ),
+                                    ),
+                                    SizedBox(width: 50),
+                                    Expanded(
+                                      child: Text(
+                                        '4',
+                                        style: TextStyle(
+                                          fontSize: 30.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(width: 50),
+                                    Expanded(
+                                      child: Text(
+                                        '4',
+                                        style: TextStyle(
+                                            fontSize: 30.0,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    SizedBox(width: 50),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    SizedBox(width: 20.0),
+                                    Expanded(
+                                      child: Text(
+                                        'المشاركات',
+                                        style:
+                                            GoogleFonts.cairo(fontSize: 15.0),
+                                      ),
+                                    ),
+                                    SizedBox(width: 50),
+                                    Expanded(
+                                      child: Text(
+                                        'المنشورات',
+                                        style:
+                                            GoogleFonts.cairo(fontSize: 15.0),
+                                      ),
+                                    ),
+                                    SizedBox(width: 50),
+                                    Expanded(
+                                      child: Text(
+                                        'الساعات',
+                                        style:
+                                            GoogleFonts.cairo(fontSize: 15.0),
+                                      ),
+                                    ),
+                                    SizedBox(width: 50),
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
-                        ]),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
