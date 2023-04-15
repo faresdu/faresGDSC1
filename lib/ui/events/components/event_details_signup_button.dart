86cdb1b2116af47268d3b9822b1533c501108858
M01010 <ihaamoodi@gmail.com>
2023-04-15 18:18:58 +0300

    implemented simple loading animation

--- lib/ui/events/components/event_details_signup_button.dart
+++ lib/ui/events/components/event_details_signup_button.dart
@@ -2,35 +2,61 @@ import 'package:flutter/material.dart';
 
 import '../../../core/utils/constants.dart';
 
-class EventDetailsSignupButton extends StatelessWidget {
+class EventDetailsSignupButton extends StatefulWidget {
   const EventDetailsSignupButton({
     Key? key,
-    this.onPressed,
+    required this.onPressed,
     required this.color,
     required this.text,
   }) : super(key: key);
-  final Function()? onPressed;
+  final Function() onPressed;
   final Color color;
   final String text;
 
   @override
+  State<EventDetailsSignupButton> createState() =>
+      _EventDetailsSignupButtonState();
+}
+
+class _EventDetailsSignupButtonState extends State<EventDetailsSignupButton> {
+  bool isLoading = false;
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onPressed,
+      onTap: () async {
+        setState(() {
+          isLoading = true;
+        });
+        await widget.onPressed();
+        setState(() {
+          isLoading = false;
+        });
+      },
       child: Container(
+        height: 50,
+        width: 150,
         padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 40),
         decoration: BoxDecoration(
-            color: color,
+            color: widget.color,
             boxShadow: Constants.shadow2,
             borderRadius: BorderRadius.circular(10)),
         child: Center(
-          child: Text(
-            text,
-            style: const TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w700,
-                color: Constants.white),
-          ),
+          child: isLoading
+              ? const SizedBox(
+                  width: 20,
+                  height: 20,
+                  child: CircularProgressIndicator(
+                    color: Constants.white,
+                  ),
+                )
+              : Text(
+                  widget.text,
+                  style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w700,
+                      color: Constants.white),
+                ),
         ),
       ),
     );
--- lib/ui/events/components/events_card_signup_button.dart
+++ lib/ui/events/components/events_card_signup_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../core/utils/constants.dart';
 
-class EventCardButton extends StatelessWidget {
+class EventCardButton extends StatefulWidget {
   const EventCardButton({
     Key? key,
     required this.onPressed,
@@ -14,31 +14,51 @@ class EventCardButton extends StatelessWidget {
   final String text;
 
   @override
+  State<EventCardButton> createState() => _EventCardButtonState();
+}
+
+class _EventCardButtonState extends State<EventCardButton> {
+  bool isLoading = false;
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: onPressed,
+        onTap: () async {
+          setState(() {
+            isLoading = true;
+          });
+          await widget.onPressed();
+          setState(() {
+            isLoading = false;
+          });
+        },
         child: Container(
           margin: const EdgeInsets.only(top: 15),
-          height: 26,
+          height: 30,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
             boxShadow: Constants.shadow5,
-            color: color,
+            color: widget.color,
           ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  text,
-                  style: const TextStyle(
-                      fontSize: 14,
-                      color: Colors.white,
-                      fontWeight: FontWeight.normal),
-                )
-              ],
-            ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              isLoading
+                  ? const SizedBox(
+                      width: 20,
+                      height: 20,
+                      child: CircularProgressIndicator(
+                        color: Constants.white,
+                      ),
+                    )
+                  : Text(
+                      widget.text,
+                      style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                          fontWeight: FontWeight.normal),
+                    )
+            ],
           ),
         ));
   }
